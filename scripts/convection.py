9ed22e67b64b6d1f03ad268126ec0662a2bb26e6
dvartany <vartanyandavid@yahoo.com>
2015-07-14 13:44:56 -0700

    rmin was rendering genSigma gap useless

--- scripts/convection.py
+++ scripts/convection.py
@@ -158,7 +158,7 @@ class Circumbinary(object):
                 np.exp(-0.5*np.square(self.r-expinit)/width**2)/(2*np.pi*self.gamma*self.r*a)
         # Make it dimensionless
         value /= self.mDisk*M/(self.gamma*a)**2
-        idxs = np.where(self.r < expinit*0.1)
+        idxs = np.where(self.r < expinit*0.7)
         value[idxs] = 0.0
         
         value = tuple(value)
