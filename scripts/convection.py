fef964856b6c94651b5488cfa3638dff437daed8
dvartany <vartanyandavid@yahoo.com>
2015-07-20 14:45:55 -0700

    bug fix alphaT

--- scripts/convection.py
+++ scripts/convection.py
@@ -213,8 +213,8 @@ class Circumbinary(object):
         if bellLin:
             @pickle_results(os.path.join(self.odir, "interpolator.pkl"))
             def buildInterpolator(r, gamma, q, fudge, mDisk, **kargs):
-                # Keep in mind that buildTemopTable() returns the log10's of the values
-                rGrid, SigmaGrid, temp, idxs = thermopy.Table(r*a*gamma, q=q, f=fudge, **kargs)
+                # Keep in mind that buildTempTable() returns the log10's of the values
+                rGrid, SigmaGrid, temp, idxs = thermopy.buildTempTable(r*a*gamma, q=q, f=fudge, **kargs)
                 # Go back to dimensionless units
                 rGrid -= np.log10(a*gamma)
                 SigmaGrid -= np.log10(mDisk*M/gamma**2/a**2)
